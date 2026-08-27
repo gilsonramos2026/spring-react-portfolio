49fcf4286f0ca6bb999dd678a0164f7cad959560
Gilson Ramos <gilsrms0@gmail.com>
2026-08-27 20:27:34 -0300

    feat: add jpa repositories layer for data persistence

---
+++ backend/src/main/java/com/portfolio/repository/CertificationRepository.java
@@ -0,0 +1,13 @@
+package com.portfolio.repository;
+
+import com.portfolio.entity.Certification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CertificationRepository extends JpaRepository<Certification, Long> {
+
+    List<Certification> findByActiveTrueOrderBySortOrderAscAndIssuedAtDesc();
+}
