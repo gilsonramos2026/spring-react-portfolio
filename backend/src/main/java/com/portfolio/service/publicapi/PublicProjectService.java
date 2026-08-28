f087352c700f3dab83d9c1ad3d53712490793c0c
Gilson Ramos <gilsrms0@gmail.com>
2026-08-28 13:52:34 -0300

    feat: add PublicProjectController and PublicProjectService

---
+++ backend/src/main/java/com/portfolio/service/publicapi/PublicProjectService.java
@@ -0,0 +1,15 @@
+package com.portfolio.service.publicapi;
+
+import com.portfolio.dto.response.ProjectResponse;
+
+import java.util.List;
+
+public class PublicProjectService {
+    public List<ProjectResponse> getProjects(Boolean featured) {
+        return null;
+    }
+
+    public ProjectResponse getProjectBySlug(String slug) {
+        return null;
+    }
+}
