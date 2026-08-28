fab306a3b6d0d63144261606d4f7f3d6c3aaafbf
Gilson Ramos <gilsrms0@gmail.com>
2026-08-28 10:42:12 -0300

    feat: add generic message response dto for clean API feedback

---
+++ backend/src/main/java/com/portfolio/dto/response/MessageResponse.java
@@ -0,0 +1,15 @@
+package com.portfolio.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageResponse {
+
+    private String message;
+}
