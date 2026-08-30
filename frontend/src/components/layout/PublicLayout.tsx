424cf0f49255c9e3ed37e07034c0cfda8e053856
Gilson Ramos <gilsrms0@gmail.com>
2026-08-30 13:04:26 -0300

    feat: stabilize PublicLayout structure and append global shell wrapper for client-facing views

--- frontend/src/components/layout/PublicLayout.tsx
+++ frontend/src/components/layout/PublicLayout.tsx
@@ -1,10 +1,12 @@
-
-const PublicLayout = () => {
+import { Outlet } from 'react-router-dom'
+import Navbar from '../navigation/Navbar'
+import Footer from '../navigation/Footer'
+export default function PublicLayout() {
   return (
-    <div>
-      
+    <div className="min-h-dvh flex flex-col">
+      <Navbar/>
+      <main className="flex-1 pt-14 sm:pt-16"><Outlet/></main>
+      <Footer/>
     </div>
   )
 }
-
-export default PublicLayout
